d7587b894a1c231faff1d4ab25bd5a91c9355911
Matthew Round <roundyz32@gmail.com>
2016-12-01 21:08:38 +0000

    added more leagues

--- src/js/custom/app.js
+++ src/js/custom/app.js
@@ -2,28 +2,137 @@ var app = angular.module('app', []);
 
 app.controller('overlay', ['$scope', '$http', function($scope, $http) {
 /* 	TODO get this dynamically updated */
-	$scope.event = {
-		name : 'darts',
-		scores : [
-			{
-				'name': 'matt', 
-				'wins': 10, 
-				'loses': 1,
-				'played': 11,
-				'win%' :0
-			},
-			{
-				'name': 'bobby', 
-				'wins': 11, 
-				'loses': 12,
-				'played': 23,
-				'win%' :0
-			}
-		]
-	};
+	$scope.events = [
+		{
+			name : 'darts',
+			scores : [
+				{
+					'name': 'matt', 
+					'wins': 10, 
+					'loses': 1,
+					'played': 11,
+				},
+				{
+					'name': 'bobby', 
+					'wins': 11, 
+					'loses': 12,
+					'played': 23,
+				},
+				{
+					'name': 'chris', 
+					'wins': 12, 
+					'loses': 12,
+					'played': 23,
+				},
+				{
+					'name': 'tom', 
+					'wins': 1, 
+					'loses': 12,
+					'played': 23,
+				},
+				{
+					'name': 'al', 
+					'wins': 1, 
+					'loses': 12,
+					'played': 23,
+				},
+				{
+					'name': 'other', 
+					'wins': 2, 
+					'loses': 12,
+					'played': 23,
+				}
+			]
+		},
+		{
+			name : 'pool',
+			scores : [
+				{
+					'name': 'matt', 
+					'wins': 10, 
+					'loses': 1,
+					'played': 11,
+				},
+				{
+					'name': 'bobby', 
+					'wins': 11, 
+					'loses': 12,
+					'played': 23,
+				},
+				{
+					'name': 'chris', 
+					'wins': 12, 
+					'loses': 12,
+					'played': 23,
+				},
+				{
+					'name': 'tom', 
+					'wins': 1, 
+					'loses': 12,
+					'played': 23,
+				},
+				{
+					'name': 'al', 
+					'wins': 1, 
+					'loses': 12,
+					'played': 23,
+				},
+				{
+					'name': 'other', 
+					'wins': 2, 
+					'loses': 12,
+					'played': 23,
+				}
+			]
+		},
+		{
+			name : 't-ten',
+			scores : [
+				{
+					'name': 'matt', 
+					'wins': 10, 
+					'loses': 1,
+					'played': 11,
+				},
+				{
+					'name': 'bobby', 
+					'wins': 11, 
+					'loses': 12,
+					'played': 23,
+				},
+				{
+					'name': 'chris', 
+					'wins': 12, 
+					'loses': 12,
+					'played': 23,
+				},
+				{
+					'name': 'tom', 
+					'wins': 1, 
+					'loses': 12,
+					'played': 23,
+				},
+				{
+					'name': 'al', 
+					'wins': 1, 
+					'loses': 12,
+					'played': 23,
+				},
+				{
+					'name': 'other', 
+					'wins': 2, 
+					'loses': 12,
+					'played': 23,
+				}
+			]
+		},
+	];
+
 
 	$scope.caculateWinPercentate = function(scoreCard) {
-		return Math.round((scoreCard.wins / scoreCard.played) *100) /100 + '%';
+		return (Math.round((scoreCard.wins / scoreCard.played) * 100) /100)*100;
 	}
+
+	$scope.date = new Date();
 
 }]);

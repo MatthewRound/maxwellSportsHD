74060c5c82f7db704d422d9db6cf208559517cd0
Matthew Round <roundyz32@gmail.com>
2016-12-10 22:41:55 +0000

    Extrated functions to services. Added countries to players.

--- src/js/custom/app.js
+++ src/js/custom/app.js
@@ -1,110 +1,35 @@
 var app = angular.module('app', []);
 
-app.controller('overlay', ['$scope', '$http', '$interval', function($scope, $http, $interval) 
-{
 
-	//TODO migrate event stuff to eventService
-	$scope.caculateWinPercentage = function(player) 
+app.service('PlayerService', function()
+{
+	this.calculateWinPercentage = function(player) 
 	{
 		return (Math.round((player.wins / player.played) * 100) /100)*100;
 	};
 
-	$scope.randomiseEvent = function()
-	{
-		var eventIndex = Math.floor(Math.random() * (events.length - 0 )) + 0;
-		$scope.currentEvent = $scope.events[eventIndex];
-		var playerIndex = Math.floor(Math.random() * ($scope.currentEvent.scores.length - 0 )) + 0;
-		$scope.currentPlayer = $scope.currentEvent.scores[playerIndex];
-	};
 
-	$scope.randomPlayer = function()
-	{
-		var playerIndex = Math.floor(Math.random() * ($scope.currentEvent.scores.length - 0 )) + 0;
-		return $scope.currentEvent.scores[playerIndex];
-	};
-
-	$scope.update = function()
-	{
-		$scope.date = new Date();
-		$scope.randomiseEvent();
-		$scope.setupVsList();
-	};
-
-	$scope.hydrate = function()
-	{
-		var ep = "/hydrate";
-		$http.get(ep, [])
-			.success(function(data, status, headers, config) {
-				eval(data);
-				$scope.events  = events;
-			})
-			.error(function(data, status, headers, config) {
-			});
-	};
-
-	$scope.init = function() 
-	{
-		$scope.events = events;
-		$scope.players = $scope.getPlayers();
-		$scope.update();
-		$interval(function(){$scope.update()}, 2000);
-		$interval(function(){$scope.hydrate()}, 7000);
-	};
-
-	$scope.setupVsList = function()
-	{
-		//TODO complete this using random player
-		$scope.vsList = [ 
-			{
-				'champ' : $scope.players.al,
-				'contender' : $scope.players.bobby
-			}
-		];
-	};
-
-	$scope.getPlayers = function() 
-	{
-		var players = {};
-		angular.forEach($scope.events, function(eventValue, eventKey){
-			angular.forEach(eventValue.scores, function(player, scoresKey){
-				players[player.name] = player;
-			});
-		});
-		return players;
-	};
-
-}]);
-
-
-app.controller('admin', ['$scope', '$http', function($scope, $http) 
-{
-
-	$scope.ok = false;
-
-	$scope.init = function() 
-	{
-		$scope.events = events;
-		$scope.players = $scope.getPlayers();
-	};
-
-
-	$scope.mergePlayers = function() 
+	this.mergePlayers = function($scope) 
 	{
 		angular.forEach($scope.events, function(eventValue, eventKey){
 			angular.forEach(eventValue.scores, function(player, scoresKey){
 				angular.forEach($scope.players, function(splayer, sscoresKey){
 					if (player.name == splayer.name) {
 						player.image = splayer.image;
+						player.country = splayer.country;
 					}
 				});
 			});
 		});
 	};
+});
 
-	$scope.save = function() 
+
+app.service('EventService', function()
+{
+	this.save = function($scope, $http)
 	{
 		var ep = "/admin";
-		$scope.mergePlayers();
 		$http.post(ep, $scope.events)
 			.success(function(data, status, headers, config) {
 				$scope.ok = true;
@@ -114,15 +39,99 @@ app.controller('admin', ['$scope', '$http', function($scope, $http)
 			});
 	};
 
-	$scope.getPlayers = function() 
+	this.randomise = function($scope) 
 	{
-		var players = {};
-		angular.forEach($scope.events, function(eventValue, eventKey){
-			angular.forEach(eventValue.scores, function(player, scoresKey){
-				players[player.name] = player;
-			});
-		});
-		return players;
+		var eventIndex = Math.floor(Math.random() * (events.length - 0 )) + 0;
+		$scope.currentEvent = $scope.events[eventIndex];
+		var playerIndex = Math.floor(Math.random() * ($scope.currentEvent.scores.length - 0 )) + 0;
+		$scope.currentPlayer = $scope.currentEvent.scores[playerIndex];
 	};
 
+	this.getPlayers = function($scope) 
+	{
+		$scope.players = {};
+		angular.forEach($scope.events, function(eventValue, eventKey){
+			angular.forEach(eventValue.scores, function(player, scoresKey){
+				$scope.players[player.name] = player;
+			});
+		});
+	};
+
+	this.randomPlayer = function($scope)
+	{
+		var playerIndex = Math.floor(Math.random() * ($scope.currentEvent.scores.length - 0 )) + 0;
+		return $scope.currentEvent.scores[playerIndex];
+	};
+
+	this.setupVsList = function($scope)
+	{
+		//TODO complete this using random player
+		$scope.vsList = [ 
+			{
+				'champ' : $scope.players.al,
+				'contender' : $scope.players.bobby
+			}
+		];
+	};
+});
+
+
+app.controller('overlay', ['$scope', '$http', '$interval', 'EventService', 'PlayerService', function($scope, $http, $interval, EventService, PlayerService) 
+{
+	$scope.updating = 0;
+
+	$scope.calculateWinPercentage = function(player) 
+	{
+		return PlayerService.calculateWinPercentage(player);
+	};
+
+	$scope.update = function()
+	{
+		$scope.date = new Date();
+		EventService.randomise($scope);
+		EventService.setupVsList($scope);
+	};
+
+	$scope.hydrate = function()
+	{
+		var ep = "/hydrate";
+		$scope.updating = 1;
+		$http.get(ep, [])
+			.success(function(data, status, headers, config) {
+				eval(data);
+				$scope.events  = events;
+				$scope.updating = 0;
+			})
+			.error(function(data, status, headers, config) {
+				$scope.updating = 0;
+			});
+	};
+
+	$scope.init = function() 
+	{
+		$scope.events = events;
+		EventService.getPlayers($scope);
+		$scope.update();
+		$interval(function(){$scope.update()}, 2000);
+		$interval(function(){$scope.hydrate()}, 7000);
+	};
+}]);
+
+
+app.controller('admin', ['$scope', '$http', 'PlayerService', 'EventService', function($scope, $http, PlayerService, EventService) 
+{
+	$scope.ok = false;
+
+	$scope.init = function() 
+	{
+		$scope.events = events;
+		EventService.getPlayers($scope);
+	};
+
+	$scope.save = function() 
+	{
+		PlayerService.mergePlayers($scope);
+		EventService.save($scope, $http)
+
+	};
 }]);
